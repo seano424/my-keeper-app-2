6a8fda0776590eeca9fd9457fecd42ea2acba58a
Sean OReilly <soreilly424@gmail.com>
2020-08-26 15:03:04 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Header() {
+    return (
+        <header>
+            <h1>Keeper App</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>This is the Note Title</h1>
+            <p>This is the Note Content</p>
+        </div>
+    )
+}
+
+export default Note
